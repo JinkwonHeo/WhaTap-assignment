cb44a395b221fd1d7e13df79c3b25706cd7d2016
JinkwonHeo <hjinkwon912@gmail.com>
2022-09-24 11:31:05 +0900

    Feat: tps상태에 timeStamp상태를 추가

--- src/reducer/action.ts
+++ src/reducer/action.ts
@@ -1,6 +1,7 @@
-import { DataActionTypes } from './types';
+import { DataActionTypes } from './actionTypes';
+import { ITpsData } from './types';
 
-export const updateTpsData = (data: number) => {
+export const updateTpsData = (data: ITpsData) => {
   return {
     type: DataActionTypes.UPDATE_TPS_DATA,
     data: data,
