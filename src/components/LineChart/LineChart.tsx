667c1558d1c9e35f21b3bf51af182317b0880a6a
JinkwonHeo <hjinkwon912@gmail.com>
2022-09-25 21:26:15 +0900

    Design: 위젯 flex 적용, LineChart gradient 적용

--- src/components/LineChart/LineChart.tsx
+++ src/components/LineChart/LineChart.tsx
@@ -3,15 +3,18 @@ import {
   axisBottom,
   axisLeft,
   line,
+  area,
   scaleLinear,
   select,
   scaleTime,
   timeMinute,
   timeFormat,
+  max,
 } from 'd3';
 import useResizeObserver from '../../hooks/useResizeOBserver';
 import styled from 'styled-components';
 import { ITps } from '../../reducer/types';
+import { Text } from '../shared/Text';
 
 export default function LineChart({
   tps,
@@ -31,6 +34,9 @@ export default function LineChart({
   const svgRef = useRef(null);
   const wrappedRef = useRef(null);
   const dimensions = useResizeObserver(wrappedRef);
+  const maxDomainValue: any = max(data, function (d) {
+    return d;
+  });
 
   useEffect(() => {
     const svg = select(svgRef.current);
@@ -43,13 +49,41 @@ export default function LineChart({
     const xScale = scaleTime().domain(xDomain).range([0, dimensions.width]);
     const yScale = scaleLinear().domain(yDomain).range([dimensions.height, 0]);
 
+    const areaGenerator = area<any>()
+      .x((value) => xScale(value.timeStamp))
+      .y0(yScale(0))
+      .y1((value) => yScale(value.data));
+
     const xAxis: any = axisBottom<Date>(xScale)
       .tickFormat(timeFormat(format))
-      .ticks(timeMinute.every(xTick));
-    const yAxis: any = axisLeft(yScale).ticks(4);
+      .ticks(timeMinute.every(xTick))
+      .tickSizeOuter(0);
+    const yAxis: any = axisLeft(yScale).ticks(5).tickSize(0).tickPadding(6).tickSizeOuter(0);
 
     svg.select('.x-axis').style('transform', `translateY(${dimensions.height}px)`).call(xAxis);
-    svg.select('.y-axis').call(yAxis);
+    svg.select('.y-axis').style('transform', 'translateX(1px)').call(yAxis);
+
+    svg
+      .append('linearGradient')
+      .attr('id', 'line-gradient')
+      .attr('gradientUnits', 'userSpaceOnUse')
+      .attr('x1', 0)
+      .attr('y1', yScale(0))
+      .attr('x2', 0)
+      .attr('y2', yScale(maxDomainValue))
+      .selectAll('stop')
+      .data([
+        { offset: '0%', color: '#81b8fc' },
+        { offset: '100%', color: '#B4EBFD' },
+      ])
+      .enter()
+      .append('stop')
+      .attr('offset', function (d) {
+        return d.offset;
+      })
+      .attr('stop-color', function (d) {
+        return d.color;
+      });
 
     svg
       .selectAll('.line')
@@ -59,29 +93,43 @@ export default function LineChart({
       .attr('d', lineGenerator)
       .attr('fill', 'none')
       .attr('stroke', '#4897F8')
-      .attr('stroke-width', 1.5);
+      .attr('stroke-width', 3);
+
+    svg
+      .selectAll('.area')
+      .data([tps.data])
+      .join('path')
+      .attr('class', 'area')
+      .attr('d', areaGenerator)
+      .attr('fill', 'url(#line-gradient)');
   }, [data, dimensions]);
 
   return (
-    <SvgWrapper ref={wrappedRef}>
-      <LineChartSvg ref={svgRef}>
-        <g className="x-axis" />
-        <g className="y-axis" />
-      </LineChartSvg>
-    </SvgWrapper>
+    <>
+      <Text>TPS</Text>
+      <SvgWrapper ref={wrappedRef}>
+        <LineChartSvg ref={svgRef}>
+          <LineChartGroup className="x-axis" />
+          <LineChartGroup className="y-axis" />
+        </LineChartSvg>
+      </SvgWrapper>
+    </>
   );
 }
 
 const SvgWrapper = styled.div`
-  max-width: 500px;
-  height: 300px;
-  margin: 0 auto;
+  padding-top: 1.5rem;
+  padding-left: 20px;
 `;
 
 const LineChartSvg = styled.svg`
   display: block;
   width: 100%;
-  height: 300px;
-  background: #f7f7f7;
+  height: 100%;
   overflow: visible;
+`;
+
+const LineChartGroup = styled.g`
+  font-family: Pretendard-light;
+  font-size: 0.8rem;
 `;
---
+++ src/components/shared/Text.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Text = styled.span`
+  font-family: Pretendard-medium;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+`;
