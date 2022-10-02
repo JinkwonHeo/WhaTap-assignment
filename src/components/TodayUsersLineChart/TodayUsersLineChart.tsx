a1f0609c9f63e8df16ad877744113549caba189b
JinkwonHeo <hjinkwon912@gmail.com>
2022-10-02 11:14:48 +0900

    Refactor: 각 위젯에서 queuing하도록 변경

--- src/components/TodayUsersLineChart/TodayUsersLineChart.tsx
+++ src/components/TodayUsersLineChart/TodayUsersLineChart.tsx
@@ -1,11 +1,13 @@
-import { useContext } from 'react';
-import { DataContext } from '../../reducer/context';
+import { useContext, useEffect } from 'react';
+import { DataContext, DispatchContext } from '../../reducer/context';
 import LineChart from '../LineChart/LineChart';
 import styled from 'styled-components';
 import { Text } from '../shared/Text';
-import { TODAY_MIDNIGHT, DAY } from '../../constants';
+import { TODAY_MIDNIGHT, DAY, MINUTE } from '../../constants';
+import { updateFetchedStatus, updateQueue } from '../../reducer/action';
 
 export default function TodayUsersLineChart() {
+  const dispatch = useContext(DispatchContext);
   const { yesterdayUsers, todayUsers } = useContext(DataContext);
   const data = todayUsers.data.map((element: number[], index: number) => [
     yesterdayUsers.data[index][0],
@@ -16,6 +18,25 @@ export default function TodayUsersLineChart() {
   const yDomain = [0, 1200];
   const tickValue = [0, 300, 600, 900, 1200];
   const isSeries = true;
+
+  useEffect(() => {
+    if (todayUsers.isFetched) {
+      setTimeout(() => {
+        dispatch(
+          updateQueue({
+            fetchType: 'series',
+            fetchName: 'todayUsers',
+            promiseAllKey: ['visitor_5m/{stime}/{etime}'],
+            params: {
+              stime: TODAY_MIDNIGHT,
+              etime: Date.now(),
+            },
+          })
+        );
+        dispatch(updateFetchedStatus(false, 'todayUsers'));
+      }, MINUTE * 5);
+    }
+  }, [todayUsers.isFetched]);
 
   return (
     <LineChartContainer>
