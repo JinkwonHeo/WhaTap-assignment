90d84b74cef9fcf2094b936c85e4220da45a2a62
JinkwonHeo <hjinkwon912@gmail.com>
2022-10-02 16:32:26 +0900

    Refactor: queuing에 필요한 객체 상수화

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,9 @@
+import { QUEUE_FORMAT } from './queueFormat';
+
 const TODAY_MIDNIGHT = new Date().setHours(0, 0, 0);
 const SECOND = 1000;
 const MINUTE = 1000 * 60;
 const HOUR = 1000 * 60 * 60;
 const DAY = 1000 * 60 * 60 * 24;
 
-export { TODAY_MIDNIGHT, DAY, HOUR, MINUTE, SECOND };
+export { TODAY_MIDNIGHT, DAY, HOUR, MINUTE, SECOND, QUEUE_FORMAT };
